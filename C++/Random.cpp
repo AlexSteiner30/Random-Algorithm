1c5c0e753cd11e0429c60518e7277718cd295224
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-01-29 11:54:56 +0100

    Update Random.cpp

--- C++/Random.cpp
+++ C++/Random.cpp
@@ -1,17 +1,36 @@
 #include <iostream>
+#include <ctime>
 
-int Random(){
-    static int x = std::time(0);
+int seed;
+
+int GenerateSeed(){
+    static int x = std::time(nullptr);
     x ^= x >> 12;
     x ^= x << 25;
     x ^= x >> 27;
     return x * 0x2545F4914F6CDD1DULL;
 }
 
-int main(){
-    std::cout << "Hello, this is a random algorithm developed by Alex Steiner!" << std::endl;
+int randomRange(int min, int max){
+    int n = max - min + 1;
+    int remainder = RAND_MAX % n;
+    int x;
+    do{
+        x = GenerateSeed();
+    } while (x >= RAND_MAX- remainder);
+    
+    if((min + x % n) < min){
+        return std::abs((min + x % n));
+    }
+    else{
+        return (min + x % n);
+    }
+}
 
-    std::cout << "Random number generate: " << Random() << std::endl;
+int main() {
+    seed = GenerateSeed();
+    
+    std::cout << randomRange(1,10) << std::endl;
 
     return 0;
 }
