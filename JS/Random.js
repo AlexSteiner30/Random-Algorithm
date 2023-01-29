96d51d2224a38551eda3851a75f39c1cb9c3a947
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-01-29 12:04:20 +0100

    Update Random.js

--- JS/Random.js
+++ JS/Random.js
@@ -1,4 +1,4 @@
-function Random(){
+function generateSeed(){
     var x = Date.now();
     x ^= x >> 12;
     x ^= x << 25;
@@ -6,4 +6,26 @@ function Random(){
     return x * 2685821657736338717;
 }
 
-console.log(Random());
+function randomRange(min, max){
+    var n = max - min + 1;
+    var remainder = Number.MAX_VALUE % n;
+    var x;
+    do{
+        x = generateSeed();
+    } while (x >= Number.MAX_VALUE - remainder);
+    
+    if((min + x % n) < min){
+        return Math.abs((min + x % n));
+    }
+    else{
+        return (min + x % n);
+    }
+}
+
+function Main() {
+    seed = generateSeed();
+    
+    console.log(randomRange(1,10));
+}
+
+Main();
