09ff306826ff7c2ff6154bc13199b34b3cac2a49
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-01-29 12:20:06 +0100

    Update Random.java

--- Java/Random.java
+++ Java/Random.java
@@ -3,17 +3,30 @@ import java.util.concurrent.TimeUnit;
 
 class RandomGenerator{
     public static void main(String[] args) {
-        System.out.println(Random());
-        System.out.println(Random());
-        System.out.println(Random());
-        System.out.println(Random());
+        System.out.println(randomRange(1,10));
     }
 
-    static long Random(){
+    static long generateSeed(){
         long x = TimeUnit.NANOSECONDS.toMicros(System.nanoTime());;
         x ^= x >> 12;
         x ^= x << 25;
         x ^= x >> 27;
         return x * 268582165;
+    }
+    
+    public static int randomRange(int min, int max){
+        int n = max - min + 1;
+        int remainder = Integer.MAX_VALUE % n;
+        int x;
+        do{
+            x = (int)generateSeed();
+        } while (x >= Integer.MAX_VALUE - remainder);
+    
+        if((min + x % n) < min){
+            return Math.abs((min + x % n));
+        }
+        else{
+            return (min + x % n);
+        }
     }
 }
